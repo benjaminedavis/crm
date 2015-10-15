4c5aefda9a353b4555b53047b20808acd8a913e6
Ben Davis <benjaminedavis@gmail.com>
2015-10-15 11:03:09 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// packages
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+})
