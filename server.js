fa05bb6bc1fc092cc26b6ee1d1b0681cfa562143
Ben Davis <benjaminedavis@gmail.com>
2015-10-17 17:53:03 -0700

    + api routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
+var User = require('./app/models/user');
 
 // connect to our database (hosted by mongolab)
 mongoose.connect('mongodb://bendavis:bendavis@ds031877.mongolab.com:31877/crm');
@@ -18,14 +19,14 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
 
 // log all requests to the console
 app.use(morgan('dev'));
 
-// Routes for API
+// Routes for our API
 // basic route for home page
 app.get('/', function(req, res) {
   res.send('Welcome to the Home Page!');
@@ -34,6 +35,17 @@ app.get('/', function(req, res) {
 // instance of the express router
 var apiRouter = express.Router();
 
+// middleware to use for all requests
+apiRouter.use(function(req, res, next) {
+      // do logging
+      console.log('Someone has arrived at our api!');
+      // we'll add more to the middleware later
+      // this is where we will authenticate users
+
+      // go to next route
+      next();
+});
+
 // test route to make sure its working
 // accessed at GET http://localhost:8080/api
 apiRouter.get('/', function(req, res) {
@@ -41,6 +53,54 @@ apiRouter.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
+apiRouter.route('/users')
+
+      // create a user POST at http://localhost:8080/api/users
+      .post(function(req, res) {
+
+          // create a new instance of the user model
+          var user = new User();
+
+          // set the user's information (comes from the request)
+          user.name = req.body.name;
+          user.username = req.body.username;
+          user.password = req.body.password;
+
+          // save the user and check for errors
+          user.save(function(err) {
+      if (err) {
+          if (err.code == 11000)
+              return res.json({ success: false, message: 'A user with that username already exists.' });
+          else
+              return res.send(err);
+          }
+              res.json({ message: 'User created!' });
+          });
+      })
+
+      // get all the users GET at http://localhost:8080/api/users
+      .get(function(req, res) {
+
+          User.find(function(err, users) {
+              if(err) res.send(err);
+
+              res.json(users);
+          });
+      });
+
+// routes that end in /users/:user_id
+apiRouter.route('/users/:user_id')
+
+      // get an individual user GET at http://localhost:8080/api/users/:user_id
+      .get(function(res, req) {
+            User.findById(req.params.user_id, function(err, user) {
+                if (err) res.send(err);
+
+
+                res.json(user);
+            });
+      })
+
 
 // Register our routes
 // all routes will be prefixed with /api
